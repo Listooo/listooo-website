6fd5608054493fb6acc2f0e756ed0ec31be1d808
GennuListoo <gennuapps@gmail.com>
2026-07-06 21:43:23 +0200

    Improve due date filtering

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3658812402" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1022846458" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
