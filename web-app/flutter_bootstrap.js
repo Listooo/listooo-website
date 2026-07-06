d4702b7c43f457297b7b041ca92c8a7b37d4b62d
GennuListoo <gennuapps@gmail.com>
2026-07-06 20:23:57 +0200

    Polish web app list cards

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "166104888" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3041639252" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
