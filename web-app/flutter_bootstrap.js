7daeef6270ab69505698941a25af2040cf6571e3
adriano.gennuso <gennu77@gmail.com>
2026-07-10 13:21:40 +0200

    Show welcome immediately after web logout

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.3ef35680.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.3b2561ac.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "601116750" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4128496193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
