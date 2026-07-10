0db6615459d6248d870f6b3899417c657e69bcac
adriano.gennuso <gennu77@gmail.com>
2026-07-10 11:14:41 +0200

    Bust web app cache for sync fix

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -33,7 +33,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.7c60bc1f.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
