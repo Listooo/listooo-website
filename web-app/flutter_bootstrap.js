350d20a634243332ced63c14e54d1272a1f8285e
adriano.gennuso <gennu77@gmail.com>
2026-07-10 11:28:04 +0200

    Polish empty states and logged out welcome

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.7c60bc1f.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.45e0bbd2.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3062249444" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "187551745" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
