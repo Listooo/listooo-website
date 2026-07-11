2e3a412e60557580facafa625636a561caedfcfd
GennuListoo <gennuapps@gmail.com>
2026-07-11 11:26:46 +0200

    Deploy shared event notifications

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=20260711-cloud-notification-history"},{}]};
+_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=20260711-shared-notification-events"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1986528752" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2008141875" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
