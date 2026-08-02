399c6a7e034ec6fe045ebf4c91d90d10f7d45d71
Adriano Gennuso <gennu@MacBook-Air.local>
2026-08-02 20:00:35 +0200

    Deploy current Flutter features to web app

--- web-app/flutter_bootstrap.js
+++ web-app/flutter_bootstrap.js
@@ -33,7 +33,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19cab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 
 const listoooWebRelease = '20260719-profile-background-editor';
